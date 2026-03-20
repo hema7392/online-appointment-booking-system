16fa460e30e4a5b29c0b30a9aeadd3c599150c79
Hemadri <142394039+hema7392@users.noreply.github.com>
2026-03-20 12:57:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(5000, () => {
-    console.log("Server started on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT);
 });
